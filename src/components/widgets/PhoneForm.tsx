ec04826eb7c3ab5a02e30a0c5c9a05d7fd362f6f
bobbystc <bobby@southernturfco.com>
2024-08-17 14:52:52 -0500

    kristy comments

--- src/components/widgets/PhoneForm.tsx
+++ src/components/widgets/PhoneForm.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PhoneForm: React.FC = () => {
   return (
-    <div className="flex flex-col items-center mx-auto max-w-72">
+    <div className="flex flex-col items-center mx-auto max-w-72 bg-slate-100 p-6 rounded-lg">
       <label htmlFor="mobilePhone" className="block text-sm font-medium mr-2 sr-only">
         Mobile Phone
       </label>
@@ -11,7 +11,7 @@ const PhoneForm: React.FC = () => {
         name="mobilePhone"
         id="mobilePhone"
         placeholder="Enter your mobile number"
-        className="py-2 px-4 block w-full text-md rounded-lg border border-gray-400 dark:border-gray-600 bg-white dark:bg-slate-900 mr-2"
+        className="py-2 px-4 block w-full text-md rounded-lg border border-gray-400 dark:border-gray-600 bg-white dark:bg-slate-900"
         required
       />
       <button type="button" className="btn-red py-4 px-4 rounded-lg w-full mt-4">
